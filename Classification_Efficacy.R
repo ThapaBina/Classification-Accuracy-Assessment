92733749c1114d812f12fa11fe39b6b8c1138910
Bina Thapa <92644272+ThapaBina@users.noreply.github.com>
2022-11-17 14:10:58 -0500

    Update Classification_Efficacy.R

--- Classification_Efficacy.R
+++ Classification_Efficacy.R
@@ -58,14 +58,3 @@ EfficacyCalc = function(Yref, Ypred, labels,...){
 }
 
 #############################################################################
-
-# Example 1:
-Yref = c("maple","maple","maple","oak","oak","pine","pine","pine","ash","ash","ash")
-Ypred = c("maple","maple","maple","oak","oak","pine","pine","pine","ash","ash","maple")
-
-EfficacyCalc(Yref = Yref, Ypred = Ypred)
-
-# Example 2: 
-Yref = c(0, 1, 1, 0, 1, 0, 0, 0)
-Ypred = c(0, 1, 1, 0, 1, 0, 0, 1)
-EfficacyCalc(Yref = Yref, Ypred = Ypred, labels = c("maple","ash"))
